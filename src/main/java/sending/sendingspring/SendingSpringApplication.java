522d39ed84d0f979d428890b5772a4dacc4e1b8c
yoongyu-hwang <dotheji11@naver.com>
2023-05-08 21:50:45 +0900

    .

--- src/main/java/sending/sendingspring/SendingSpringApplication.java
+++ src/main/java/sending/sendingspring/SendingSpringApplication.java
@@ -2,8 +2,12 @@ package sending.sendingspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@Configuration
+@ImportResource("classpath:databaseContext.xml")
 public class SendingSpringApplication {
 
 	public static void main(String[] args) {
