ece23b6530797e3b67c38da97572e0332fb5326f
Hwang-YoonGyu <dotheji11@naver.com>
2023-05-13 23:50:32 +0900

    Update UserVO.java

--- src/main/java/com/codingquokka/bottle/vo/UserVO.java
+++ src/main/java/com/codingquokka/bottle/vo/UserVO.java
@@ -7,12 +7,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserVO {
-    private String USER_ID;
-    private String USER_PW;
-    private String IS_CERTIFIED;
-    private int CNT_RECEIVED;
-    private String JOIN_DT;
-    private String RESTRICTION_DT;
-    private String BELONG;
-    private int IS_USING;
+    private String userId;
+    private String userPw;
+    private String isCertified;
+    private int cntReceived;
+    private String joinDt;
+    private String restrictionDt;
+    private String belong;
+    private int isUsing;
 }
