99098db806a302c5e1457ccf9f0b4848ada25db0
Hwang-YoonGyu <dotheji11@naver.com>
2023-08-16 21:33:48 +0900

    쏘링

--- src/main/java/com/codingquokka/bottle/dao/ChatContentDao.java
+++ src/main/java/com/codingquokka/bottle/dao/ChatContentDao.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class chatDao {
+public class ChatContentDao {
 
     @Autowired
     SqlSession sqlSession;
 
-    private static String namepace = "mapper.chatMapper";
+    private static String namepace = "mapper.chatContentMapper";
 
 }
